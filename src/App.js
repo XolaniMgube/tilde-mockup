f680557db68467a9c9860c0c8c8659f4e90f0549
XolaniMgube <xnmgube@gmail.com>
2020-10-19 11:49:38 +0200

    components show onclick using vanilla js for now

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import './App.css';
 import Interface from './components/Interface'
 import YourFeedback from './components/YourFeedback'
 
+function yourFeedback() {
+  var element = document.getElementById("your-feedback")
+  element.classList.remove("your-feedback")
+}
+
+function interfaceFun() {
+  var element = document.getElementById("interface")
+  element.classList.remove("your-feedback")
+}
+
+
 function App() {
   return (
+
     <div className="App">
+
+      <button onClick={interfaceFun}>Interface</button>
+      <button onClick={yourFeedback}>Your feedback</button>
+
+      <div className="your-feedback" id="interface">
       <Interface 
         totalReviews="20"
         agreements="14"
         disagreements="6"
         accuracy="70%"
       />
+      </div>
 
-      <YourFeedback />
+      <div className="your-feedback" id="your-feedback">
+        <YourFeedback/>
+      </div>
+      
     </div>
   );
 }
